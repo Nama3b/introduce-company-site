9e5015284c22d89138bd4329d614880aebf44007
Le Thanh Long <leethanhlong2210@gmail.com>
2023-04-28 17:26:33 +0700

    update laravel ui and auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::prefix('admin')->group(function () {
+    Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::get('lohin', [LoginController::class, 'login']);
+    Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+});
+
+Route::get('/home', function () {
+    return view('admin.home');
+})->name('home')->middleware('auth');
+
+Route::middleware(['auth:web'])->prefix('admin')->group(function () {
+    Route::get('user/new', [UserController::class, 'new']);
 });
